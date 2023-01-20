96302b24314752893394aa73127ce4a1bbab2afe
NPavlovaaa <n.pavlova.web@gmail.com>
2023-01-20 23:50:02 +0500

    rewriting CharInfo component using custom hook

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,18 +1,17 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './charInfo.scss';
-import MarvelService from "../../services/MarvelService";
+import гыуMarvelService from "../../services/MarvelService";
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import Skeleton from "../skeleton/Skeleton";
+import useMarvelService from "../../services/MarvelService";
 
 const CharInfo = (props) => {
 
     const [char, setChar] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
 
-    const marvelService = new MarvelService();
+    const {loading, error, getCharacter} = useMarvelService();
 
     useEffect(() =>{
         updateChar();
@@ -24,26 +23,12 @@ const CharInfo = (props) => {
         if (!charId){
             return;
         }
-        onCharLoading();
-        marvelService
-            .getCharacter(charId)
+        getCharacter(charId)
             .then(onCharLoaded)
-            .catch(onError)
-
-    }
-
-    const onCharLoading = () =>{
-        setLoading(true);
-    }
-
-    const onError = () =>{
-        setError(true);
-        setLoading(false);
     }
 
     const onCharLoaded = (char) =>{
         setChar(char);
-        setLoading(false);
     }
 
     const skeleton = char || loading || error ? null : <Skeleton/>;
