4daaeac451ad3d16377f63192e1901e2bdd5f481
NPavlovaaa <n.pavlova.web@gmail.com>
2023-01-25 01:13:27 +0500

    creating component pages and adding routing v5+

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './charInfo.scss';
-import гыуMarvelService from "../../services/MarvelService";
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage'
 import Skeleton from "../skeleton/Skeleton";
